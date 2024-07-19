2ef52b912fa4c528dfea6b72410241e614be451d
CristianN01 <cristiannechifor71@gmail.com>
2024-07-20 01:17:37 +0200

    rappresentato i dati in pagina con un ul

--- resources/views/trains/index.blade.php
+++ resources/views/trains/index.blade.php
@@ -7,4 +7,32 @@
         Trains
     </h1>
     @dump($trains)
+    <ul>
+        @foreach ($trains as $train)
+            <li>
+                <p>
+                    Tipo treno: <em>{{ $train->azienda }}</em>
+                </p>
+                <p>
+                    Partenza da: <em>{{ $train->stazione_di_partenza }}</em>
+                </p>
+                <p>
+                    Arrivo a: <em>{{ $train->stazione_di_arrivo }}</em>
+                </p>
+                <p>
+                    CODICE: <strong>{{ $train->codice_treno }}</strong>
+                </p>
+                <p>
+                    N carrozze: <em>{{ $train->numero_carrozze }}</em>
+                </p>
+                <p>
+                    In orario: <em>{{ $train->in_orario }}</em>
+                </p>
+                <p>
+                    <em>{{ $train->cancellato }}</em>
+                </p>
+
+            </li>
+        @endforeach
+    </ul>
 @endsection
